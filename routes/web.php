44cc63551bde41c04e110a6f7b213fb5aad806ce
samuel <samuelnjoya08@gmail.com>
2025-07-11 14:39:33 +0100

    import de la bd pour deploiement

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,21 @@ use Illuminate\Support\Facades\Route;
 //     return view('auth.login');
 // });
 
+Route::get('/import-sql', function () {
+    $path = database_path('scripts/parc_auto.sql');
+
+    if (!file_exists($path)) {
+        return "❌ Le fichier SQL est introuvable.";
+    }
+
+    try {
+        DB::unprepared(file_get_contents($path));
+        return "✅ Base de données importée avec succès.";
+    } catch (\Exception $e) {
+        return "❌ Erreur : " . $e->getMessage();
+    }
+});
+
 Route::get('/',[AuthController::class,'login']);
 Route::get('logout',[AuthController::class, 'logout']);
 Route::post('/',[AuthController::class, 'auth_login']);
